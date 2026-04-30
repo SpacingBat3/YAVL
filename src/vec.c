ddb0118200a27b0f0bc726454bf2e225c483cdbf
SpacingBat3 <spacingbat3@gmail.com>
2026-04-30 07:29:34 +0200

    feat(project): major reworks
    Refactor CMake, replace vec internals and API: expose struct fields
    (data, allign,
    len, reservd), add vec_fromarray/vec_toarray, initial guards sectioning
    for further optimization experimentations and fix multiple memory,
    sizing and boundary bugs in push,
    pop, fit, set, get and free operations.

--- src/vec.c
+++ src/vec.c
@@ -22,137 +22,155 @@
 
 #include "namespace.h"
 
-typedef struct {
-  void* data;
-  size_t allignment;
-  size_t size;
-  size_t reservd;
-} NS(vec_t);
-
 #include <YAVL/vec.h>
 
-NS(vec_res_t) NS(vec_init)(NS(vec_t) *const memory, size_t data_align, size_t data_reserv) {
-  if(memory==NULL) return NS_UPPER(VEC_RES_NULL);
-  memory->size=0;
-  memory->reservd=(!data_reserv)?5:data_reserv;
-  memory->allignment=(!data_align)?1:data_align;
-  if(memory->data) free(memory->data);
-  memory->data=calloc(memory->reservd, memory->allignment);
-  return memory->data == NULL ? NS_UPPER(VEC_RES_OOM) : NS_UPPER(VEC_RES_OK);
+NS(vec_res_t) NS( vec_init )(NS(vec_t) *const vec, size_t data_align, size_t data_reserv) {
+  #ifndef YAVL_FAST
+  if(vec==NULL) return NS_UPPER(VEC_RES_NULL);
+  if(vec->data&&vec->allign) free(vec->data);
+  #endif
+  vec->len=0;
+  vec->allign=(!data_align)?1:data_align;
+  vec->reservd=(!data_reserv)?data_align:data_reserv;
+  vec->data=calloc(vec->reservd, vec->allign);
+  return vec->data == NULL ? NS_UPPER(VEC_RES_OOM) : NS_UPPER(VEC_RES_OK);
 }
 
-NS(vec_res_t) NS(vec_push)(NS(vec_t) *const vec, const void *const data) {
+NS(vec_res_t) NS( vec_fromarray )(NS(vec_t) *const vec, void *const array, const size_t alignment, const size_t length) {
+    #ifndef YAVL_FAST
+    if(vec==NULL || array==NULL) return NS_UPPER(VEC_RES_NULL);
+    if(vec->data&&vec->allign) free(vec->data);
+    #endif
+    vec->len=(vec->reservd=length);
+    vec->data=vec;
+    return NS_UPPER(VEC_RES_OK);
+}
+
+NS(vec_res_t) NS( vec_push )(NS(vec_t) *const vec, const void *const data) {
+  #ifndef YAVL_FAST
   if(vec==NULL) return NS_UPPER(VEC_RES_NULL);
-  if(vec->size >= vec->reservd) {
-    vec->reservd+=5;
-    void *const new = realloc(vec->data, vec->reservd*vec->allignment);
+  #endif
+  if(vec->len >= vec->reservd) {
+    vec->reservd*=2;
+    void *const new = realloc(vec->data, vec->reservd*vec->allign);
     if(new) vec->data = new;
     else return NS_UPPER(VEC_RES_OOM);
   }
-  if(!memcpy(vec->data+((vec->size++)*vec->allignment),
-      data,vec->allignment)) {
-    --vec->size;
-    return NS_UPPER(VEC_RES_FAIL);
+  if(memcpy(vec->data+((vec->len++)*vec->allign),
+      data,vec->allign)) {
+    return NS_UPPER(VEC_RES_OK);
   }
-  return NS_UPPER(VEC_RES_OK);
+  --vec->len;
+  return NS_UPPER(VEC_RES_FAIL);
 }
 
-NS(vec_errorable_t) NS(vec_pop)(NS(vec_t) *const vec) {
+NS(vec_errorable_t) NS( vec_pop )(NS(vec_t) *const vec) {
+  #ifndef YAVL_FAST
   if(vec==NULL) return (NS(vec_errorable_t)){
       .status=NS_UPPER(VEC_RES_NULL),
       .mem=NULL
   };
-  if(vec->size == 0) return (NS(vec_errorable_t)){
+  #endif
+  if(vec->len == 0) return (NS(vec_errorable_t)){
       .status=NS_UPPER(VEC_RES_FAIL),
       .mem=NULL
   };
   return (NS(vec_errorable_t)){
     .status=NS_UPPER(VEC_RES_OK),
-    .mem=vec->data+((--vec->size)*vec->allignment)
+    .mem=vec->data+((--vec->len)*vec->allign)
   };
 }
 
-NS(vec_res_t) NS(vec_fit)(NS(vec_t)* const vec) {
+NS(vec_res_t) NS( vec_fit )(NS(vec_t)* const vec) {
+  #ifndef YAVL_FAST
   if(vec == NULL) return NS_UPPER(VEC_RES_NULL);
   // Already alligned
-  if(vec->reservd == vec->allignment) return NS_UPPER(VEC_RES_OK);
+  if(vec->reservd == vec->len) return NS_UPPER(VEC_RES_OK);
+  #endif
   // Resize memory allocated to memory taken
-  void *const new = realloc(vec->data, vec->size*vec->allignment);
+  void *const new = realloc(vec->data, vec->len*vec->allign);
   // Realloc check for very bad allocators
   if(!new) return NS_UPPER(VEC_RES_OOM);
-  vec->allignment=vec->size;
+  vec->reservd=vec->len;
   vec->data = new;
   // We're good
   return NS_UPPER(VEC_RES_OK);
 }
 
-NS(vec_res_t) NS(vec_set)(NS(vec_t)* const vec, size_t offset, const void *const data) {
+NS(vec_res_t) NS( vec_set )(NS(vec_t)* const vec, size_t offset, const void *const data) {
+  #ifndef YAVL_FAST
   // Simple bound checks
   if(vec == NULL) return NS_UPPER(VEC_RES_NULL);
-  if(offset >= vec->size) return NS_UPPER(VEC_RES_FAIL);
+  if(offset >= vec->len) return NS_UPPER(VEC_RES_FAIL);
+  #endif
   // Copy memory
   return (NS(vec_res_t) /* VEC_RES_FAIL == 1 */) !memcpy(
-    vec->data+(offset*vec->allignment),
+    vec->data+(offset*vec->allign),
     data,
-    vec->allignment
+    vec->allign
   );
 }
 
-NS(vec_res_t) NS(vec_get)(NS(vec_t)* const vec, size_t offset, void *const out) {
+NS(vec_res_t) NS( vec_get )(NS(vec_t)* const vec, size_t offset, void *const out) {
+  #ifndef YAVL_FAST
   // Simple bound checks // FIXME: opt oportunities
   if(vec == NULL) return NS_UPPER(VEC_RES_NULL);
-  if(offset >= vec->size) return NS_UPPER(VEC_RES_FAIL);
-
+  if(offset >= vec->len) return NS_UPPER(VEC_RES_FAIL);
+  #endif
   // Copy memory
   return (NS(vec_res_t) /* VEC_RES_FAIL == 1 */) !memcpy(
     out,
-    vec->data+(offset*vec->allignment),
-    vec->allignment
+    vec->data+(offset*vec->allign),
+    vec->allign
   );
 }
 
-NS(vec_errorable_t) NS(vec_ref)(NS(vec_t)* const vec) {
-  if(vec == NULL) return (NS(vec_errorable_t)){
-    .status=NS_UPPER(VEC_RES_NULL), .mem=NULL
-  };
-  return (NS(vec_errorable_t)) {
-    .status=NS_UPPER(VEC_RES_OK), .mem=vec->data
-  };
-}
-
-bool NS(vec_chkptr)(const NS(vec_t)* const vec, const void *const ptr) {
+bool NS( vec_chkptr )(const NS(vec_t)* const vec, const void *const ptr) {
   return
     // Test boundaries
-    ptr > vec->data && ptr < vec->data+(vec->allignment*vec->reservd) &&
+    ptr > vec->data && ptr < vec->data+(vec->allign*vec->reservd) &&
     // Test allignment
-    (((ptrdiff_t)(ptr-vec->data)) % vec->allignment) == 0;
+    (((ptrdiff_t)(ptr-vec->data)) % vec->allign) == 0;
 };
 
-NS(vec_errorable_t) NS(vec_flush)(NS(vec_t)* const vec) {
-  NS(vec_res_t) status;
-  switch(status = NS(vec_fit)(vec)) {
+NS(vec_errorable_t) NS( vec_toarray )(NS(vec_t)* const vec, size_t *const final_len) {
+  #ifdef YAVL_FAST
+  NS(vec_res_t) status = vec->len==vec->reservd ? NS_UPPER(VEC_RES_OK) : NS_UPPER(VEC_RES_OOM)
+  #else
+  NS(vec_res_t) status = NS(vec_fit)(vec);
+  if(final_len == NULL) status = NS_UPPER(VEC_RES_NULL);
+  switch(status) {
     // OOM when fitting means bad allocator
     // This is not critical.
     case NS_UPPER(VEC_RES_OOM):
     case NS_UPPER(VEC_RES_OK): {
+      #endif
+      // Logical array size
+      (*final_len) = vec->len;
       void* arr = vec->data;
       vec->data = NULL;
-      vec->size=0;
+      vec->len=0;
       vec->reservd=0;
-      vec->allignment=0;
+      vec->allign=0;
       return (NS(vec_errorable_t)){status, .mem=arr};
+      #ifndef YAVL_FAST
     } break;
     default:
       return (NS(vec_errorable_t)){status, .mem=NULL};
-  };
+  }
+  #endif
 }
 
-NS(vec_res_t) NS(vec_free)(NS(vec_t)* const vec) {
+NS(vec_res_t) NS( vec_free )(NS(vec_t)* const vec) {
+  #ifndef YAVL_FAST
   if(vec==NULL) return NS_UPPER(VEC_RES_NULL);
-  vec->size=0;
+  if(vec->data && vec->allign) free(vec->data);
+  vec->len=0;
   vec->reservd=0;
-  vec->allignment=0;
-  if(vec->data) free(vec->data);
+  vec->allign=0;
   vec->data=NULL;
+  #else
+  free(vec->data);
+  #endif
   return NS_UPPER(VEC_RES_OK);
 }
